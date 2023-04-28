667172996a794c152cbd6f8cae1f34e511c5317d
matteo-guenci <mguenci@gmail.com>
2023-04-28 18:35:18 +0200

    Update classes.py

--- progetto/classes/classes.py
+++ progetto/classes/classes.py
@@ -35,11 +35,13 @@ class Manifest(EntityWithMetadata):
 class Canvas(EntityWithMetadata):
     pass
 
-annotation_1 = Annotation('https://dl.ficlit.unibo.it/iiif/2/28429/annotation/p0001-image','"https://dl.ficlit.unibo.it/iiif/2/45498/full/699,800/0/default.jpg"','https://dl.ficlit.unibo.it/iiif/2/28429/canvas/p1,painting')
-annotation_2 = Annotation('https://dl.ficlit.unibo.it/iiif/2/28429/annotation/p0002-image','"https://dl.ficlit.unibo.it/iiif/2/45499/full/699,800/0/default.jpg"','https://dl.ficlit.unibo.it/iiif/2/28429/canvas/p2,painting')
 
-print("The objects in 'annotation_1' and 'annotation_2' share the same class -->", type(annotation_1) == type(annotation_2))
-print("Indeed, the types of the two objects are both", type(annotation_1))
 
-print("\nThe objects in 'annotation_1' and 'annotation_2' are the same object -->", id(annotation_1) == id(annotation_2))
-print("Indeed, the integers identifying the two objects are", id(annotation_1), "and", id(annotation_2), "respectively")
+# annotation_1 = Annotation('https://dl.ficlit.unibo.it/iiif/2/28429/annotation/p0001-image','"https://dl.ficlit.unibo.it/iiif/2/45498/full/699,800/0/default.jpg"','https://dl.ficlit.unibo.it/iiif/2/28429/canvas/p1,painting')
+# annotation_2 = Annotation('https://dl.ficlit.unibo.it/iiif/2/28429/annotation/p0002-image','"https://dl.ficlit.unibo.it/iiif/2/45499/full/699,800/0/default.jpg"','https://dl.ficlit.unibo.it/iiif/2/28429/canvas/p2,painting')
+
+# print("The objects in 'annotation_1' and 'annotation_2' share the same class -->", type(annotation_1) == type(annotation_2))
+# print("Indeed, the types of the two objects are both", type(annotation_1))
+
+# print("\nThe objects in 'annotation_1' and 'annotation_2' are the same object -->", id(annotation_1) == id(annotation_2))
+# print("Indeed, the integers identifying the two objects are", id(annotation_1), "and", id(annotation_2), "respectively")
