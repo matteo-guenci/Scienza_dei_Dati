244164cc19a189d2491408caadbdc3250b43729d
matteo-guenci <mguenci@gmail.com>
2023-05-26 17:49:15 +0200

    Update relational.py

--- progetto/Relational/relational.py
+++ progetto/Relational/relational.py
@@ -1,5 +1,6 @@
 from pandas import read_csv, merge, Series, DataFrame
 from sqlite3 import connect
+import pandas as pd
 
 annotations = read_csv("data/annotations.csv", keep_default_na=False, dtype={"id":"string",
                                                                              "body":"string",
@@ -14,38 +15,41 @@ metadata = read_csv("data/metadata.csv", keep_default_na=False, dtype={"id":"str
                                                                        "title":"string",
                                                                        "creator":"string"})
 creator = metadata[["creator"]]
+# if ";" in creator:
+#     creator = creator.split(";")
+#     for i in creator:
+        
 metadata_internal_id = []
 for idx, row in creator.iterrows():
     metadata_internal_id.append(str(idx))
 
-creator.insert(0, "", Series(metadata_internal_id, dtype="string"))
+creator.insert(0, "EntityWithMetadataCreatorID", Series(metadata_internal_id, dtype="string"))
     
 # venues_ids.insert(0, "venueId", Series(venue_internal_id, dtype="string"))
 
-print(metadata_internal_id)
+# print(metadata_internal_id)
 print(creator)
+word_to_find = "collection"
+# collection = metadata.query("id.str.contains(@word_to_find, case=False)")
+#print(collection)
 
+collection = pd.DataFrame()
+manifest = pd.DataFrame()
+canvas = pd.DataFrame()
+for word , row in metadata.iterrows():
+    if "collection" in row["id"]:
+        collection = collection.append(row)   #filtered_df = pd.concat([filtered_df, row], axis=1)
+    if "manifest" in row["id"]:
+        manifest = manifest.append(row)
+    if "canvas" in row["id"]:
+        canvas = canvas.append(row)        
+# print(collection)
+# print(manifest)
+# print(canvas)
 
-# venues = read_csv("../01/01-venues.csv", 
-#                   keep_default_na=False,
-#                   dtype={
-#                       "id": "string",
-#                       "name": "string",
-#                       "type": "string"
-#                   })
-
-# # This will create a new data frame starting from 'venues' one,
-# # and it will include only the column "id"
-# venues_ids = venues[["id"]]
-
-# # Generate a list of internal identifiers for the venues
-# venue_internal_id = []
-# for idx, row in venues_ids.iterrows():
-#     venue_internal_id.append("venue-" + str(idx))
-
-# # Add the list of venues internal identifiers as a new column
-# # of the data frame via the class 'Series'
-# venues_ids.insert(0, "venueId", Series(venue_internal_id, dtype="string"))
-
-# # Show the new data frame on screen
-# venues_ids
+#ora tocca fare le merge
+df_joined = merge(collection, creator, left_on="creator", right_on="creator") 
+print(df_joined)
+collection = df_joined[["id", "title", "EntityWithMetadataCreatorID"]]
+collection = collection.rename(columns={"EntityWithMetadataCreatorID" : "internalId"})
+print(collection)
