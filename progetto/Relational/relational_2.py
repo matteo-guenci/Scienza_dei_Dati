54056ff197fc8e98c1712a933b15e19bf8775d0e
matteo-guenci <mguenci@gmail.com>
2023-06-15 16:03:54 +0200

    16:03

--- progetto/Relational/relational_2.py
+++ progetto/Relational/relational_2.py
@@ -18,7 +18,14 @@ metadata = read_csv("data/metadata.csv", keep_default_na=False, dtype={"id":"str
 
 image = annotations[["body"]]
 image.insert(0, "imageID", Series(annotations["body"].apply(extract_id), dtype="string"))
+image = image.rename(columns={"body":"image_url"})
+    # annotations_j = annotations_j.rename(columns={"internalID_x":"internalID", "id_x":"id", "internalID_y":"target"})
 
+df_joined = merge(annotations, image, left_on="body", right_on="image_url")
+annotations = df_joined[["id", "imageID", "target", "motivation"]]
+annotations = annotations.rename(columns={"imageID":"body"})
+print(image)
+print(annotations)
 metadata.insert(0, "internalID", Series(metadata["id"].apply(extract_id), dtype="string"))
 
 
@@ -44,11 +51,25 @@ for word, row in metadata.iterrows():
     if "canvas" in row["id"]:
         canvas = canvas._append(row[["id", "internalID"]]._append(Series({"manifestID":manifest_id, "collectionID":collection_id})), ignore_index=True)
 
-df_joined = merge(collection, creator, on="internalID", how="left")
-df_joined_2 = merge(manifest, creator, on="internalID", how="left")
-df_joined_3 = merge(canvas, creator, on="internalID", how="left")
+# df_joined = merge(collection, creator, on="internalID", how="left")
+# df_joined_2 = merge(manifest, creator, on="internalID", how="left")
+# df_joined_3 = merge(canvas, creator, on="internalID", how="left")
 
-print(df_joined)
-print(df_joined_2)
-print(df_joined_3)
+# print(df_joined)
+# print(df_joined_2)
+# print(df_joined_3)
 
+with connect("annotations_metadata_2.db") as con:
+    creator.to_sql("Creator", con, if_exists="replace", index=False)
+    collection.to_sql("Collection", con, if_exists="replace", index=False)
+    manifest.to_sql("Manifest", con, if_exists="replace", index=False)
+    canvas.to_sql("Canvas", con, if_exists="replace", index=False)
+    image.to_sql("Image", con, if_exists="replace", index=False)
+    annotations.to_sql("Annotation", con, if_exists="replace", index=False)
+    # df_joined.to_sql("DFJoined_1", con, if_exists="replace", index=False)
+    # df_joined_2.to_sql("DFJoined_2", con, if_exists="replace", index=False)
+    # df_joined_3.to_sql("DFJoined_3", con, if_exists="replace", index=False)
+    # df_joined_4.to_sql("DFJoined_4", con, if_exists="replace", index=False)
+    # df_joined_5.to_sql("DFJoined_5", con, if_exists="replace", index=False)
+    # df_joined_6.to_sql("DFJoined_6", con, if_exists="replace", index=False)
+    con.commit()
