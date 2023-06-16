b1887130a7099c1adaf7661cfafbf4a287edf222
matteo-guenci <mguenci@gmail.com>
2023-06-16 12:59:16 +0200

    12:59

--- progetto/Relational/relational_2.py
+++ progetto/Relational/relational_2.py
@@ -8,6 +8,19 @@ def extract_id(s):               #aggiunto
         return None
     else:
         return pattern
+def is_part_of(x, y, type_1, type_2):
+    x = x.strip("/" + type_1)
+    y = y.strip("/" + type_2)
+    x = x.split("/")
+    y = y.split("/")
+    if type_2 == "manifest":
+        if y[1] in x[0]:
+            return True
+    if type_2 == "canvas":
+        if y[1] in x[1] and y[0] in x[0]:
+            return True
+        
+                
 annotations = read_csv("data/annotations.csv", keep_default_na=False, dtype={"id":"string",
                                                                              "body":"string",
                                                                              "target":"string",
@@ -24,15 +37,12 @@ image = image.rename(columns={"body":"image_url"})
 df_joined = merge(annotations, image, left_on="body", right_on="image_url")
 annotations = df_joined[["id", "imageID", "target", "motivation"]]
 annotations = annotations.rename(columns={"imageID":"body"})
-<<<<<<< HEAD
 # print(image)
-print(annotations)
-=======
->>>>>>> 96d66ce7596781ab18da21f96bde8407ffb82092
+# print(annotations)
 metadata.insert(0, "internalID", Series(metadata["id"].apply(extract_id), dtype="string"))
 
 
-creator = metadata[["creator", "title", "internalID"]] 
+creator = metadata[["creator", "internalID"]] 
 
 # creator.insert(0, "EntityWithMetadataCreatorID", Series(internal_ids, dtype="string"))  
 #ma per creare "creator" non basterebbe prendere metadata selezionando solo id, internal id e creator e fare il rename su internalID? senza fare tutti questi procedimenti di metti e togli
@@ -44,24 +54,99 @@ canvas = DataFrame()
 collection_id = ""
 manifest_id = ""
 canvas_id = ""
+
 for word, row in metadata.iterrows():
     if "collection" in row["id"]:
         collection_id = row["internalID"]
-        collection = collection.append(row[["id", "internalID"]])
+        collection = collection._append(row[["id", "internalID", "title"]])
     if "manifest" in row["id"]:
         manifest_id = row["internalID"]
-        manifest = manifest.append(row[["id", "internalID"]].append(Series({"collectionID":collection_id})), ignore_index=True)
+        manifest = manifest._append(row[["id", "internalID", "title"]])
     if "canvas" in row["id"]:
-        canvas = canvas.append(row[["id", "internalID"]].append(Series({"manifestID":manifest_id, "collectionID":collection_id})), ignore_index=True)
-
+        canvas = canvas._append(row[["id", "internalID", "title"]])
 # df_joined = merge(collection, creator, on="internalID", how="left")
 # df_joined_2 = merge(manifest, creator, on="internalID", how="left")
 # df_joined_3 = merge(canvas, creator, on="internalID", how="left")
+collection_items = DataFrame()
+manifest_items = DataFrame()
+# collection_items = merge(collection, manifest, left_on=None, right_on=None, left_index=False, right_index=False, how='inner', sort=False)
+# for word, row in metadata.iterrows():
+#     if "collection" in row["id"]:
+#         collection_id = row["internalID"]
+#         collection = collection.append(row[["id", "internalID"]])
+#     if "manifest" in row["id"]:
+#         manifest_id = row["internalID"]
+#         manifest = manifest.append(row[["id", "internalID"]].append(Series({"collectionID":collection_id})), ignore_index=True)
+#     if "canvas" in row["id"]:
+#         canvas = canvas.append(row[["id", "internalID"]].append(Series({"manifestID":manifest_id, "collectionID":collection_id})), ignore_index=True)
 
-print(collection)
-print(manifest)
-print(canvas)
+# Create empty DataFrames to store the results
+collection_items = pd.DataFrame()
+manifest_items = pd.DataFrame()
 
+# Iterate over rows in 'collection' DataFrame
+for index, row in collection.iterrows():
+    # Iterate over rows in 'metadata' DataFrame
+    for index_2, row_2 in metadata.iterrows():
+        if "manifest" in row_2["id"]:
+            # print (row["id"], row_2["id"], is_part_of(row["internalID"], row_2["internalID"], "collection", "manifest"))
+            if is_part_of(row["internalID"], row_2["internalID"], "collection", "manifest"):
+                # Create a temporary DataFrame with the desired values
+                temp_df = DataFrame({"collection_id": [row["id"]], "manifest_id": [row_2["id"]]})
+                # Append the temporary DataFrame to 'collection_items'
+                collection_items = collection_items._append(temp_df)
+collection_items = collection_items.reset_index(drop=True)
+
+# Iterate over rows in 'manifest' DataFrame
+for index, row in manifest.iterrows():
+    # Iterate over rows in 'metadata' DataFrame
+    for index_2, row_2 in metadata.iterrows():
+        if "canvas" in row_2["id"]:
+            if is_part_of(row["internalID"], row_2["internalID"], "manifest", "canvas"):
+                # Create a temporary DataFrame with the desired values
+                temp_df = DataFrame({"manifest_id": [row["id"]], "canvas_id": [row_2["id"]]})
+                # Append the temporary DataFrame to 'manifest_items'
+                manifest_items = manifest_items._append(temp_df)
+manifest_items = manifest_items.reset_index(drop=True)
+
+# for word, row in collection.iterrows():
+#     for word_2, row_2 in metadata.iterrows():
+#         if "manifest" in row_2["id"]:
+#             print(is_part_of(row["id"], row_2["id"], "collection", "manifest"))
+#             if is_part_of(row["id"], row_2["id"], "collection", "manifest"):
+#                 collection_items = collection_items._append(row["id"])
+#                 collection_items = collection_items._append(row_2["id"])
+#                 # collection_items= collection_items._append(row[["id"], row_2[["id"]]])
+# for word, row in manifest.iterrows():
+#     for word_2, row_2 in metadata.iterrows():
+#         if "canvas" in row_2["id"]:
+#             if is_part_of(row["id"], row_2["id"], "manifest", "canvas"):
+#                 manifest_items = manifest_items._append(row["id"])
+#                 manifest_items = manifest_items._append(row_2["id"])
+#                 # manifest_items = manifest_items._append(row[["id"], row_2[["id"]]])
+
+
+            
+    
+    
+    # if "collection" in row["id"]:
+    #     for word_2, row_2 in metadata.iterrows():
+    #         if "manifest" in row_2["id"]:
+    #             if is_part_of(row["id"], row_2["id"], "collection", "manifest"):
+    #                 collection_items._append(row["id"])
+            
+    # if "manifest" in row["id"]:
+    #     for word_2, row_2 in metadata.iterrows():
+    #         if "canvas" in row_2["id"]:
+    #             if is_part_of(row["id"], row_2["id"], "manifest", "canvas"):
+    #                 manifest_items._append(row["id"])
+# metadata.insert(0, "internalID", Series(metadata["id"].apply(extract_id), dtype="string"))
+# print(collection)
+# print(manifest)
+# print(canvas)
+print(collection_items)
+
+print(manifest_items)
 # with connect("annotations_metadata_2.db") as con:
 #     creator.to_sql("Creator", con, if_exists="replace", index=False)
 #     collection.to_sql("Collection", con, if_exists="replace", index=False)
