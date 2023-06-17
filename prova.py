eebd43dc6c49e173fc5a79506ed6d047d3ec080e
matteo-guenci <mguenci@gmail.com>
2023-06-17 17:11:07 +0200

    17:11

--- prova.py
+++ prova.py
@@ -1,14 +1,14 @@
 from impl import *
 # Then, create the RDF triplestore (remember first to run the
 # Blazegraph instance) using the related source data
-# grp_endpoint = "http://127.0.0.1:9999/blazegraph/sparql"
-# col_dp = CollectionProcessor()
-# col_dp.setDbPathOrUrl(grp_endpoint)
-# col_dp.uploadData("././data/collection-1.json")
-# col_dp.uploadData("././data/collection-2.json")
+grp_endpoint = "http://127.0.0.1:9999/blazegraph/sparql"
+col_dp = CollectionProcessor()
+col_dp.setDbPathOrUrl(grp_endpoint)
+col_dp.uploadData("data/collection-1.json")
+col_dp.uploadData("data/collection-2.json")
 
-# grp_qp = TriplestoreQueryProcessor()
-# grp_qp.setDbPathOrUrl(grp_endpoint)
+grp_qp = TriplestoreQueryProcessor()
+grp_qp.setDbPathOrUrl(grp_endpoint)
 
 
 # print (grp_qp.getAllCanvases())
@@ -33,5 +33,5 @@ generic.addQueryProcessor(rel_qp)
 result = generic.getAllAnnotations()
 for i in result:
     print(i.print_ann())
-print(rel_qp.getEntityById("https://dl.ficlit.unibo.it/iiif/2/28429/annotation/p0001-image"))
+# print(rel_qp.getEntityById("https://dl.ficlit.unibo.it/iiif/2/28429/annotation/p0001-image"))
 
